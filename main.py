54de4b8c30e2dac656b7f4ff4a470ec2b9f353e8
Muhammad Umar Javed <muhammadumerjaved44@gmail.com>
2022-05-27 20:39:19 +0500

    turn off main function

--- main.py
+++ main.py
@@ -36,5 +36,5 @@ async def create_user(data=Body(...)):
     # return data
 
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000)
